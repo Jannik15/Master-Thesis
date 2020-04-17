53f590eea92702af19b3380ce27f0b8328655739
Thomas Boelt <thomas@boelthansen.dk>
2020-04-17 12:32:51 +0200

    Doors in VR integration

---
+++ P10-UP/Assets/ButtonTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onButtonPressed;
+
+    private bool pressedInProgress = false;
+
+    private PlayerInteractions playerInteractions;
+
+    void Start()
+    {
+        playerInteractions = FindObjectOfType<PlayerInteractions>();
+        if (playerInteractions == null)
+        {
+            Debug.Log("no playerInteractions");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Button" && !pressedInProgress)
+        {
+            pressedInProgress = true;
+            playerInteractions.DoorAction(other);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Button")
+        {
+            pressedInProgress = false;
+        }
+    }
+
+
+}
--- P10-UP/Assets/DoorLock.cs
+++ P10-UP/Assets/DoorLock.cs
@@ -1,20 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorLock : MonoBehaviour
 {
     public bool isLocked, beenUnlocked = false;
+
+   
+    public GameObject buttonTrigger;
+
+    private Vector3 originalPosition;
+
+    private float minDistance, maxDistance;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        minDistance = Vector3.Distance(buttonTrigger.transform.position, transform.position);
+
+        maxDistance = buttonTrigger.transform.position.y;
+
+        originalPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(buttonTrigger.transform.position, transform.position) >= minDistance)
+        {
+            transform.position = originalPosition;
+        }
+
+        if (transform.position.y <= maxDistance)
+        {
+            transform.position = new Vector3(transform.position.x, maxDistance, transform.position.z);
+        }
     }
 
 }
