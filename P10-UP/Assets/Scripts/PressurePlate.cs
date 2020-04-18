30520fc674e4e5ade8cc70da0239aa24c8dac74f
Jannik15 <jneerd15@student.aau.dk>
2020-04-18 17:26:01 +0200

    Added a pressure plate using spring joint

---
+++ P10-UP/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    void OnTriggerEnter(Collider plate)
+    {
+        if (plate.CompareTag("Button"))
+        {
+            Debug.Log("Pressure plate entered");
+        }
+    }
+    void OnTriggerExit(Collider plate)
+    {
+        if (plate.CompareTag("Button"))
+        {
+            Debug.Log("Pressure plate exited");
+        }
+    }
+}
