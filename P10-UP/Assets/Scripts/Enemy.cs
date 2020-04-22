eface773231d4917a3ce78f5262815b4625302f0
Thomas Boelt <thomas@boelthansen.dk>
2020-04-22 09:28:42 +0200

    Gun Dispenser and Mechanics

---
+++ P10-UP/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float Health = 10f;
+
+    public void TakeDamage(float amount)
+    {
+        Health -= amount;
+        if (Health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
