d4d8cc5a125650555f36d037ce0808ba75083389
Jannik15 <jneerd15@student.aau.dk>
2020-05-10 23:59:51 +0200

    Fixed pressure plate with a harder limit on y axis
    Tried using configureable joint, but it didn't work like a spring with the settings i tried so instead went with the simpler but less efficient hard-cap on y axis through update.

---
+++ P10-UP/Assets/Scripts/LockWorldPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class LockWorldPosition : MonoBehaviour
+{
+    [SerializeField] private float y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= y)
+        {
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        }
+    }
+}
