a03676c359cd034b00d6249e67ae8c4821830819
Thomas Boelt <thomas@boelthansen.dk>
2020-05-13 23:34:43 +0200

    Tutorial + Movement intro!
    Still needs player pref

---
+++ P10-UP/Assets/CenterButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterButton : MonoBehaviour
+{
+    public GameObject nextButton, greyButton;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            nextButton.SetActive(true);
+            greyButton.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            nextButton.SetActive(false);
+            greyButton.SetActive(true);
+        }
+
+    }
+
+}
