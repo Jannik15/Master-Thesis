1f02a41eab1dd912b091fae456d9016231f9a248
Jannik15 <jneerd15@student.aau.dk>
2020-05-14 10:17:22 +0200

    Added playerprefs for movement type in Main

---
+++ P10-UP/Assets/Scripts/MainStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainStart : MonoBehaviour
+{
+    [SerializeField] private Button naturalWalkingBtn, steeringBtn;
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("MovementType", 0) == 0) // 0 = Natural Walking, 1 = Steering
+        {
+            naturalWalkingBtn.onClick.Invoke();
+            Debug.Log("Started Main scene with movement type of 0 = Natural Walking");
+        }
+        else
+        {
+            steeringBtn.onClick.Invoke();
+            Debug.Log("Started Main scene with movement type of 1 = Steering");
+        }
+    }
+}
