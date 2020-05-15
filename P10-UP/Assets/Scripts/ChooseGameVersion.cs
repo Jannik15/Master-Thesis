e753bc064dd406c0ed23f38d5d04529e258c9514
Thomas Boelt <thomas@boelthansen.dk>
2020-05-15 03:57:53 +0200

    Last fixes before Publish

--- P10-UP/Assets/Scripts/ChooseGameVersion.cs
+++ P10-UP/Assets/Scripts/ChooseGameVersion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChooseGameVersion : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class ChooseGameVersion : MonoBehaviour
     {
         PlayerPrefs.SetInt("Testing", 1);
         PlayerPrefs.SetInt("MovementType", Random.Range(0, 2)); // 0 = Natural Walking, 1 = Steering
+        Debug.Log("Random movement type was: " + PlayerPrefs.GetInt("MovementType"));
         SceneManager.LoadScene("TestStart");
     }
 
